c3999e5c44d7767fe202073e94153d410a61974e
asadbekzaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2025-05-19 20:45:14 +0500

    Bot commands

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { TelegrafModule } from 'nestjs-telegraf';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { TelegrafModule, InjectBot } from 'nestjs-telegraf';
 import { options } from 'src/config/telegraf.options';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
+import { Telegraf } from 'telegraf';
 
 @Module({
   imports: [TelegrafModule.forRootAsync(options()), UserModule, AdminModule],
 })
-export class BotModule {}
+export class BotModule implements OnModuleInit {
+  constructor(@InjectBot() private readonly bot: Telegraf) {}
+
+  async onModuleInit() {
+    await this.bot.telegram.setMyCommands([
+      { command: 'start', description: 'Botni ishga tushurish' },
+    ]);
+  }
+}
